6960099e029b4a0f081a655c2355b361822196bd
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-01-02 19:05:53 +0530

    used arrow fn

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -6,16 +6,25 @@ import './App.css'
 function App() {
 
   let [counter,setCounter] = useState(0)
-  function plusfn()
-  {
-    counter++  
-    setCounter(counter)
-  }
-  function minusfn()
-  {
-    counter-- 
-    setCounter(counter)
-  }
+  // function plusfn()
+  // {
+  //   counter++  
+  //   setCounter(counter)
+  // }
+  // function minusfn()
+  // {
+  //   if(counter!=0)
+  //   counter-- 
+  //   setCounter(counter)
+  // }
+    const plusfn = () => {
+      counter++;
+      setCounter(counter);
+    }
+    const minusfn = () => {
+      counter--;
+      setCounter(counter);
+    }
   return (
     <>
       <h1>02 Counter project</h1>
