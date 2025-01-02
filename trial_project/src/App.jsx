bbb78c0dfe8ba43a116df5fcbb086bf18a297ac3
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-01-02 13:52:55 +0530

    tried newfn

--- trial_project/src/App.jsx
+++ trial_project/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import Newfn from './newjs'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Shijin react</h1>
+    <>
+    <Newfn/>
+    <h1>Shijin</h1>
+    </>
+    
   )
 }
 
---
+++ trial_project/src/newjs.jsx
@@ -0,0 +1,8 @@
+function Newfn()
+{
+    return(
+        <h2>This is from the new jsx file</h2>
+    )
+}
+
+export default Newfn
