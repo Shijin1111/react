2899c4ccc2ae005eb02c395eeed3253c3a6f5a1e
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-01-04 22:04:41 +0530

    done

--- fun_react/src/App.jsx
+++ fun_react/src/App.jsx
@@ -7,28 +7,29 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{width:500,margin: '0 auto', 
+      padding: '20px',
+      backgroundColor: '#f9f9f9', 
+      border: '1px solid #ccc',
+      borderRadius: '8px',
+      boxShadow: '0 4px 8px rgba(0, 0, 1, 0.1)',}}>
+      <img src={reactLogo} width={100} />
+      
+      <h2>Fun facts about React</h2>
+      <ul style={{listStyleType:'none'}}>
+          <li>React was created by Jordan Walke, a software engineer at Facebook, in 2011.</li>
+          <li>It was first used for Facebook's News Feed and later for Instagram in 2012.</li>
+          <li>React uses a virtual DOM to improve performance by minimizing direct manipulations of the real DOM.</li>
+          <li>The library follows a component-based architecture, making code reusable and easier to manage.</li>
+          <li>React introduced the concept of hooks in version 16.8, simplifying state and side-effect management in functional components.</li>
+          <li>It's maintained by Facebook, but it has a massive open-source community contributing to its growth.</li>
+          <li>React powers major websites and applications, including Netflix, Airbnb, and WhatsApp Web.</li>
+          <li>JSX (JavaScript XML) is a unique syntax used in React to write HTML-like code within JavaScript.</li>
+          <li>React Native, a framework for building mobile apps, is based on React and allows developers to use a single codebase for iOS and Android.</li>
+          <li>React has a strong ecosystem with tools like React Router, Redux, and Next.js, making it suitable for a wide range of applications.</li>
+      </ul>
+
+    </div>
   )
 }
 
