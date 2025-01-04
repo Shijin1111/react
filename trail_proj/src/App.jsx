0c3e6d4bb3d1ac7cb338f9ef49bb08630a36a0fb
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-01-04 21:43:05 +0530

    fun_react

--- trail_proj/src/App.jsx
+++ trail_proj/src/App.jsx
@@ -1,10 +1,65 @@
-function App()
-{
-  return(
+import { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    // Disable scrolling by applying styles directly to body
+    document.body.style.overflow = 'hidden';
+    
+    // Clean up when the component is unmounted
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
+  return (
     <>
-        <h1>shijin</h1>    
+      <div
+        style={{
+          position: 'relative',
+          height: '100vh',
+          overflow: 'hidden',
+        }}
+      >
+        <img
+          src="23.jpg"
+          alt="Background"
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+            zIndex: -1,
+          }}
+        />
+        <div>
+          <h1>Shijin</h1>
+          <p style={{color:'white',
+            fontSize:20}}><b>Welcome to My Homepage
+Use the menu to select different Stylesheets
+
+Stylesheet 1
+Stylesheet 2
+Stylesheet 3
+Stylesheet 4
+No Stylesheet
+Same Page Different Stylesheets
+This is a demonstration of how different stylesheets can change the layout of your HTML page. You can change the layout of this page by selecting different stylesheets in the menu, or by selecting one of the following links:
+Stylesheet1, Stylesheet2, Stylesheet3, Stylesheet4.
+
+No Styles
+This page uses DIV elements to group different sections of the HTML page. Click here to see how the page looks like with no stylesheet:
+No Stylesheet.
+
+Side-Bar
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praes</b></p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
