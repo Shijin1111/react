8ad055090f56881a3d5163f161eff4e98183be7e
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-01-05 20:49:09 +0530

    header done

---
+++ travel_blog/src/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+function Header(){
+    return (
+        <>
+        <div className="Header">
+            <img src='./globe.jpg' />
+            <h2>My travel journel</h2>
+        </div>
+        </>
+    )
+}
+export default Header
