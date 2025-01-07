bfe0ecace6920b03121cf8825c0770266a1cb2c7
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-01-07 21:29:00 +0530

    js checking

--- chef/src/Header.jsx
+++ chef/src/Header.jsx
@@ -1,10 +1,13 @@
 // import chefClaudeLogo from "./images/chef-claude-icon.png"
 
 export default function Header() {
+    function handleMouseOver() {
+        console.log("I was hovered");
+    }
     return (
         <header>
             <img src=" "/>
-            <h1>Chef Claude</h1>
+            <h1 onMouseOver={handleMouseOver}>Chef Claude</h1>
         </header>
     )
 }
