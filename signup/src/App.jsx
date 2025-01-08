158f7f8f5ead52bf9957065adab7f0966462bccb
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-01-08 09:26:45 +0530

    signup done

---
+++ signup/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  function signUp(event) {
+    event.preventDefault(); // Prevent the default form submission
+    const formdata = new FormData(event.currentTarget);
+    const email = formdata.get("email");
+    const password = formdata.get("pswd");
+    console.log("Email:", email);
+    console.log("Password:", password);
+  }
+
+  return (
+    <>
+      <h1>Sign up</h1>
+      <div className="container">
+        <form onSubmit={signUp}>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="pswd">Password:</label>
+            <input type="password" id="pswd" name="pswd" />
+          </div>
+          <div className="form-group">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
