3f586f5f2038adb37557cb1fa2dc4ef24c4ae0a2
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-01-08 17:43:38 +0530

    toggled using ternary

--- jokes/src/Joke.jsx
+++ jokes/src/Joke.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
+
 export default function Joke(props)
 {
+
+    const [isShown,setisShown] = useState(false)
+
+    function toggle()
+    {
+        setisShown(pre=>!pre)
+        console.log(isShown)
+    }
+
     return (
         <>
         <hr></hr>
-            <h2>{props.setup}</h2>
-            <p>{props.punchline}</p>
+            { props.setup && <h2> {props.setup} </h2> }
+            {isShown && <p> {props.punchline} </p> }
+            <button onClick={toggle}>{isShown?"hide":"show"}</button>
         </>
     )
 }
