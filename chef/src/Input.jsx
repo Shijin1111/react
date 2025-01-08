4466243ba06def7bd7c2b38327d3f8142547fc51
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-01-08 08:46:14 +0530

    chef completed

--- chef/src/Input.jsx
+++ chef/src/Input.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react"
 export default function Input() {
-    const ingredients = ["a","b","c"]
-    const ingredientsList = ingredients.map(ingredient=>(
-        <li key={ingredients}>{ingredient}</li>
+    const [ingredients,setingredients] = useState([])
+
+
+    const ingredientsList = ingredients.map((ingredient,index)=>(
+        <li key={index}>{ingredient}</li>
     ))
     const [result,fn] = useState("Yes")
+
+    function handleSubmit(event)
+    {
+        event.preventDefault()
+        const formdata = new FormData(event.currentTarget)
+        const newingredient = formdata.get("ingredient")
+        setingredients(prevIngredients => [...prevIngredients,newingredient])
+
+    }
+
+    
+
+
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form  onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
                     type="text"
+                    name="ingredient" // Added name attribute
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
                 />
